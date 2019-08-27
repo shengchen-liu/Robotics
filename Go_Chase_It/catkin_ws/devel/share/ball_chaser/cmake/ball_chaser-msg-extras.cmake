843f4e44005d787bc73376de63a796531057b552
root <liu.sheng@gatech.edu>
2019-08-27 21:28:01 +0000

    finish Project2

--- Go_Chase_It/catkin_ws/devel/share/ball_chaser/cmake/ball_chaser-msg-extras.cmake
+++ Go_Chase_It/catkin_ws/devel/share/ball_chaser/cmake/ball_chaser-msg-extras.cmake
@@ -1,2 +1,2 @@
 set(ball_chaser_MESSAGE_FILES "")
-set(ball_chaser_SERVICE_FILES "/home/workspace/Udacity_Robotics/catkin_ws/src/ball_chaser/srv/DriveToTarget.srv")
+set(ball_chaser_SERVICE_FILES "/home/workspace/Udacity_Robotics/Go_Chase_It/catkin_ws/src/ball_chaser/srv/DriveToTarget.srv")
