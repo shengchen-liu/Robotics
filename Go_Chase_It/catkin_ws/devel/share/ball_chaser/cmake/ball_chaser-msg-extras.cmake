8b72b0ba1cb7d5c68fc96ea843f9ad23c4fcf9bc
lawson1225 <liu.sheng@husky.neu.edu>
2019-08-29 12:28:56 -0400

    Chage the structure

--- Go_Chase_It/catkin_ws/devel/share/ball_chaser/cmake/ball_chaser-msg-extras.cmake
+++
@@ -1,2 +0,0 @@
-set(ball_chaser_MESSAGE_FILES "")
-set(ball_chaser_SERVICE_FILES "/home/workspace/Udacity_Robotics/Go_Chase_It/catkin_ws/src/ball_chaser/srv/DriveToTarget.srv")
--- Go_Chase_It/catkin_ws/devel/share/simple_arm/cmake/simple_arm-msg-extras.cmake
+++
@@ -1,2 +0,0 @@
-set(simple_arm_MESSAGE_FILES "")
-set(simple_arm_SERVICE_FILES "/home/robond/Udacity_Robotics/catkin_ws/src/simple_arm/srv/GoToPosition.srv")
