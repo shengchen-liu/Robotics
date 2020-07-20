2453a32bd9327d9e11f15b48d08a27f6d0acb789
shengchen-liu <shengchen.liu1225@gmail.com>
2020-07-20 16:54:22 -0400

    delete notes

--- catkin_ws/devel/share/ball_chaser/cmake/ball_chaser-msg-extras.cmake
+++
@@ -1,2 +0,0 @@
-set(ball_chaser_MESSAGE_FILES "")
-set(ball_chaser_SERVICE_FILES "/home/shengchen/Udacity_Robotics/catkin_ws/src/ball_chaser/srv/DriveToTarget.srv")
--- catkin_ws/devel/share/robot_pose_ekf/cmake/robot_pose_ekf-msg-extras.cmake
+++
@@ -1,2 +0,0 @@
-set(robot_pose_ekf_MESSAGE_FILES "")
-set(robot_pose_ekf_SERVICE_FILES "/home/shengchen/Udacity_Robotics/catkin_ws/src/RoboND-EKFLab/robot_pose_ekf/srv/GetStatus.srv")
